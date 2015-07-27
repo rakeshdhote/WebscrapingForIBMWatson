4ed02ded9519e295d6237efa05f58be14098d591
Rakesh Dhote <rakesh.dhote@gmail.com>
2015-07-27 15:08:47 -0400

    Python Script

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# WebscrapingForIBMWatson
-WebscrapingForIBMWatson
+# Web Scraping Script for the IBM Watson  
+
+The repo consists of the Python web scraping script to extract 126 parks information from the [OntarioParks.ca](OntarioParks.ca) website. 
+
+The script writes parks information in the `Microsoft Word` formatted document. This document serves an input to train the `IBM Watson` for predicting the appropriate park based on the asked question.
+
+The script uses the following Python packages:
+*  BeautifulSoup  
+*  Requests  
+*  docx.
+
+The project is for educational purpose and not intended to use for any commercial applications. 
+
+
+
---
+++ WebScrapingScript.py
@@ -0,0 +1,94 @@
+from bs4 import BeautifulSoup
+import requests
+from docx import Document
+
+base_html = 'http://www.ontarioparks.com'
+home_html = base_html + '/en'
+
+## the function parses web sections
+def www_section_parser(soup, key, value, elem):
+    parks_temp1 = soup.find(attrs={key: value})
+    parks_temp2 = parks_temp1.find_all(elem)
+    return parks_temp2
+
+## Extract parks name and url in dictionary
+def parks_information(home_html):
+    r = requests.get(home_html)
+    soup = BeautifulSoup(r.content)
+
+    temp_parks = www_section_parser(soup, 'class','btn-group','li')
+
+    parks_list = [] # list of dictionaries
+    for item in temp_parks:
+        parks = {}
+        parks['name'] = item.text
+        parks['url_ext'] = item.find('a').get('href')
+        parks_list.append(parks)
+    return parks_list
+
+## Extract each park information in the park list
+def scrape_parks(parks_list):
+    document = Document() # Open Word Document
+
+    for i in range(0,len(parks_list)):
+
+        parkname = parks_list[i]['name']
+        park_url = base_html + parks_list[i]['url_ext']
+
+        print parkname
+
+        r = requests.get(park_url)
+        soup = BeautifulSoup(r.content)
+
+        ## Park Introduction
+        temp_intro = www_section_parser(soup, 'id','tabs-introduction','li')
+
+        park_intro = []
+        for item in temp_intro:
+            park_intro.append(item.text)
+
+        ## Park Camping
+        temp_camping = www_section_parser(soup, 'id','tabs-camping','p')
+
+        park_camping = []
+        for item in temp_camping:
+            park_camping.append(item.text)
+
+        ## Park - Things to Do
+        temp_tod = www_section_parser(soup, 'id','tabs-thingstodo','p')
+
+        park_thingstodo = []
+        for item in temp_tod:
+            park_thingstodo.append(item.text)
+
+        ## Park - Amenities
+        temp_amenities = www_section_parser(soup, 'id','tabs-amenities','p')
+
+        park_amenities = []
+        for item in temp_amenities:
+            park_amenities.append(item.text)
+
+        ### Populating  Document
+        document.add_heading(parkname, level=1)
+        for i in park_intro:
+            document.add_paragraph(i) #, style='ListBullet')
+
+        for i in park_camping:
+            document.add_paragraph(i)
+
+        for i in park_thingstodo:
+            document.add_paragraph(i)
+
+        for i in park_amenities:
+            document.add_paragraph(i)
+
+        document.add_paragraph('          ')
+    #    document.add_page_break()
+
+    document.save('OntarioParks.docx')
+
+##
+if __name__ == "__main__":
+    parks_list = parks_information(home_html)
+    scrape_parks(parks_list)
+    print '## Open the document: OntarioParks.docx ##'
