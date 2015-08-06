76f8b84172677b60a0fffd7bc39806a2865a95bf
Rakesh Dhote <rakesh.dhote@gmail.com>
2015-08-05 23:15:00 -0400

    Python script updated to write a two-level sub-heading formatted Microsoft Word document

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Web Scraping Script for the IBM Watson  
 
-The repo consists of the Python web scraping script to extract 126 parks information from the [OntarioParks.ca](OntarioParks.ca) website. 
+The repo consists of the Python web scraping script to extract 126 parks information from the [OntarioParks.com](OntarioParks.com) website. 
 
 The script writes parks information in the `Microsoft Word` formatted document. This document serves an input to train the `IBM Watson` for predicting the appropriate park based on the asked question.
 
--- WebScrapingScript.py
+++ WebScrapingScript.py
@@ -5,20 +5,21 @@ from docx import Document
 base_html = 'http://www.ontarioparks.com'
 home_html = base_html + '/en'
 
-## the function parses web sections
+#%% the function parses web sections
 def www_section_parser(soup, key, value, elem):
     parks_temp1 = soup.find(attrs={key: value})
     parks_temp2 = parks_temp1.find_all(elem)
     return parks_temp2
 
-## Extract parks name and url in dictionary
+#%% Extract parks name and url in dictionary
 def parks_information(home_html):
     r = requests.get(home_html)
     soup = BeautifulSoup(r.content)
 
     temp_parks = www_section_parser(soup, 'class','btn-group','li')
 
-    parks_list = [] # list of dictionaries
+    # Create list of park dictionaries
+    parks_list = [] 
     for item in temp_parks:
         parks = {}
         parks['name'] = item.text
@@ -26,7 +27,7 @@ def parks_information(home_html):
         parks_list.append(parks)
     return parks_list
 
-## Extract each park information in the park list
+#%% Extract each park information in the park list
 def scrape_parks(parks_list):
     document = Document() # Open Word Document
 
@@ -68,11 +69,11 @@ def scrape_parks(parks_list):
         for item in temp_amenities:
             park_amenities.append(item.text)
 
-        ### Populating  Document
+        ### Populating Word Document
         document.add_heading(parkname, level=1)
         document.add_heading('Introduction', level=2)
         for i in park_intro:
-            document.add_paragraph(i) #, style='ListBullet')
+            document.add_paragraph(i) 
 
         document.add_heading('Camping in '+parkname, level=2)
         for i in park_camping:
@@ -89,10 +90,10 @@ def scrape_parks(parks_list):
         document.add_paragraph('          ')
     #    document.add_page_break()
 
-    document.save('Camp Grounds in Ontario.docx')
+    document.save('Camp_Grounds_in_Ontario.docx')
 
-##
+#%%
 if __name__ == "__main__":
     parks_list = parks_information(home_html)
     scrape_parks(parks_list)
-    print '## Open the document: Camp Grounds in Ontario.docx ##'
+    print '## Open the document: Camp_Grounds_in_Ontario.docx ##'
